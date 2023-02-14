3830cfe59a62ec962c9e7f07e01a1633735f5fc9
Aysegul <aysglbabacan@gmail.com>
2023-02-14 14:06:56 +0300

    ödev

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Server'ı buradan başlatın
+const server = require("./api/server");
+
+const port = process.env.PORT || 9000;
+
+server.listen(port, () => {
+  console.log(`Running on ${port}`);
+});
